20712a00858e84ce8a98fe10be8d6828d567fe99
Jose Carlos Pazos <jpazos@wolfram.com>
2022-01-27 18:48:14 -0500

    reworked first task for better use of callNumber

--- main.js
+++ main.js
@@ -1,29 +1,51 @@
 const random = require("./utils");
 const myArgs = process.argv.slice(2);
 const rolls = myArgs[0] || 1;
-let n = 76;
+/*let n = 76;
 let arr = [...Array(n).keys()];
 arr.shift();
-n = 75;
+n = 75;*/
 
-const callNumber = (iters = 1) => {
+const generateArray = () => {
+  let n = 76;
+  let arr = [...Array(n).keys()];
+  arr.shift();
+  return arr;
+};
+
+const callNumber = (arr) => {
+  let n = arr.length;
   let randomPosition;
   let randomValue;
-  for (let i = 0; i < iters; i++) {
+  /*for (let i = 0; i < iters; i++) {
     randomPosition = random.getRandomInt(n);
     randomValue = arr.splice(randomPosition, 1);
     console.log("new number!", randomValue[0]);
     n--;
+  }*/
+  randomPosition = random.getRandomInt(n);
+  randomValue = arr.splice(randomPosition, 1);
+  console.log("new number!", randomValue[0]);
+  n--;
+};
+
+const generateCard = (name) => {
+  console.log("generating card for", name);
+  let card = [[]];
+  for (let i = 0; i < 5; i++) {
+    card[0].push(random.getRandomInt(16));
   }
 };
 
 const main = () => {
-  callNumber(rolls);
+  let arr = generateArray();
+  for (let i = 0; i < rolls; i++) {
+    callNumber(arr);
+  }
   console.log("done!");
   console.log(arr);
 };
 
-console.log(myArgs);
 if (require.main === module) {
   main();
 }
